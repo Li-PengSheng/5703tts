a69f48fb747e9bfbf500e0a2f08a55b9d1e6b99b
Myuli <94470095+Li-PengSheng@users.noreply.github.com>
2026-09-22 16:20:39 +1000

    shell lacked package

--- scripts/setup_cloud_environment.sh
+++ scripts/setup_cloud_environment.sh
@@ -16,7 +16,7 @@ readonly HIGGS_MODEL_REVISION="0056125158f940389ab0808a581b8b2c590b32d4"
 readonly HIGGS_MODEL_SHA256="2f7965264c360b38180885006944aa16bd1de20f4e6cff79f6473bfcf8ae3d5a"
 readonly -a BASE_PACKAGES=(
   git curl wget ca-certificates python3 ffmpeg sox libsndfile1
-  build-essential pkg-config iproute2 mokutil
+  build-essential pkg-config iproute2 mokutil pciutils
 )
 readonly -a UCX_PACKAGES=(
   autoconf automake libtool make gcc g++ flex bison m4 libnuma-dev
